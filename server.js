5b842f16aeae90c0c679569f7d52e55c62e940f1
eddy-reynoso <reynosoeddy@gmail.com>
2020-09-28 15:16:01 -0400

    UI build

--- server.js
+++ server.js
@@ -11,20 +11,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const requestLogger = (request, response, next) => {
-    console.log("Method: ", request.method);
-    console.log("Path: ", request.path);
-    console.log("Body", request.body);
-    //console.log("Headers", request.headers);
-    console.log("---");
-    next();
+  console.log("Method: ", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body", request.body);
+  //console.log("Headers", request.headers);
+  console.log("---");
+  next();
 };
 
 app.use(requestLogger);
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+  res.send("Hello world");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
+
 app.listen(port, function() {
-    console.log("Listening on port", port);
+  console.log("Listening on port", port);
 });
